a51749ae768b6b6df049b6c0f9d59054ed850659
Rifqi Fajar Ramdani <rifqifajar24@gmail.com>
2022-12-28 05:46:52 +0700

    Create Quiz Automation Test Case Day 6

---
+++ QuizDay6/Libs/internal/GlobalVariable.groovy
@@ -0,0 +1,30 @@
+package internal
+
+import com.kms.katalon.core.configuration.RunConfiguration
+import com.kms.katalon.core.main.TestCaseMain
+
+
+/**
+ * This class is generated automatically by Katalon Studio and should not be modified or deleted.
+ */
+public class GlobalVariable {
+     
+    /**
+     * <p>Profile default : URL Website</p>
+     */
+    public static Object G_URL
+     
+
+    static {
+        try {
+            def selectedVariables = TestCaseMain.getGlobalVariables("default")
+			selectedVariables += TestCaseMain.getGlobalVariables(RunConfiguration.getExecutionProfile())
+            selectedVariables += TestCaseMain.getParsedValues(RunConfiguration.getOverridingParameters())
+    
+            G_URL = selectedVariables['G_URL']
+            
+        } catch (Exception e) {
+            TestCaseMain.logGlobalVariableError(e)
+        }
+    }
+}
---
+++ QuizDay6/Test Suites/Inventory.ts
@@ -0,0 +1,48 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Membeli barang inventory</description>
+   <name>Inventory</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>c5a9636f-48f3-4ff1-a494-564a57dde833</testSuiteGuid>
+   <testCaseLink>
+      <guid>cec12db3-6be1-424d-ad57-446d508e3dcc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Checkout/TWC01_Verify checkout item</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>62ed9ce0-b618-458a-8714-a8f0a739fce3</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Inventory</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>62ed9ce0-b618-458a-8714-a8f0a739fce3</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>firstname</value>
+         <variableId>42b037ce-22e6-4d4a-bf37-f762905e5460</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>62ed9ce0-b618-458a-8714-a8f0a739fce3</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>lastname</value>
+         <variableId>0b3033c9-3f4e-4170-aab3-94dd4f6d1f62</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>62ed9ce0-b618-458a-8714-a8f0a739fce3</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>zipcode</value>
+         <variableId>d66ce9f9-8ef3-46a0-a3d1-73e75d71a6f4</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
